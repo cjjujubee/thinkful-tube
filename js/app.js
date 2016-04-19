c097652eeb7540d904614ec017d84a0b49b36fc1
OoogleBoogle <simattfield@gmail.com>
2016-04-19 11:17:39 -0700

    Adding thumbnail images to HTML

--- js/app.js
+++ js/app.js
@@ -22,9 +22,42 @@ function getRequest(searchTerm){
     //   console.log(value.title);
     // });
     // console.log(data.items[0].snippet.title);
-    console.log(data);
-    var videoTitle = data.items[0].snippet.title
-    var videoId = data.items[0].id.videoId; //gets videoId
+    // console.log(myData);
+    // var videoTitle = data.items[0].snippet.title
+    // var videoId = data.items[0].id.videoId; //gets videoId
+    var videoObjects = [];
+    myData.map(function(item) {
+      var id = item.id.videoId;
+      var title = item.snippet.title;
+      var thumbnail = item.snippet.thumbnails.url;
+
+      videoObjects.push({id: id, title: title, thumbnail: thumbnail});
+    })
+    // console.log(videoObjects);
+    return videoObjects;
   });
 }
+
+var buildThumbnailHtml = function(videoObjects) {
+  console.log(videoObjects);
+  for (var i = 0; i < videoObjects.length; i++) {
+    $('#searchResults').append('<img src="' + videoObjects[i].thumbnail + '">')
+  }
+}
+
+$(function() {
+  $('#searchTerm').on('submit', function(e) {
+    e.preventDefault();
+    var searchResults = getRequest($('#query').val());
+    console.log(searchResults);
+    buildThumbnailHtml(searchResults);
+  })
+})
+
+
+
+
+
+
+
 
